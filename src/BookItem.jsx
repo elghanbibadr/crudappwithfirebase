2341fee27cc110cf9fc11b006529e3252ed5020a
badr ghanbi <bghanbi50@gmail.com>
2023-05-25 13:10:27 +0100

    style the action buttons

--- src/BookItem.jsx
+++ src/BookItem.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const BookItem = ({id,title,author,status,rank}) => {
   return (
     <tr>
-    <td className='text-center border-[1px] p-2'>{rank}</td>
+    <td className='text-center border-[1px] p-2'>{rank + 1}</td>
     <td className='text-center border-[1px] p-2'>{author}</td>
     <td className='text-center border-[1px] p-2'>{title}</td>
     <td className='text-center border-[1px] p-2'>{status}</td>
     <td className='text-center border-[1px] p-2'>
-        <button>edit</button>
-        <button>delete</button>
+        <button className='bg-red-400 text-white px-3 py-1 font-medium rounded-md'>Edit</button>
+        <button className='bg-gray-900 text-white mx-1 px-3 py-1 font-medium rounded-md'>Delete</button>
     </td>
     </tr>
   )
